748a90bfc0ccf057852454b9d6dbbf01f4087642
Abhishek Dutta <abhi97.dutta@gmail.com>
2022-08-06 04:36:54 +0530

    add updates

--- src/components/PlanList.vue
+++ src/components/PlanList.vue
@@ -5,9 +5,14 @@
     <tr>
       <th scope="col">#</th>
       <th scope="col">Recommended Activities</th>
+      <th scope="col"></th>
     </tr>
   </thead>
   <tbody>
+    <tr><td>1</td><td>Choose a place to walk that is safe, such as your local mall.</td></tr>
+        <tr><td>2</td><td>Jogging in the Morning</td></tr>
+        <tr><td>3</td><td>Brisk Stretching</td></tr>
+        
         <tr v-for="item in list" v-bind:key="item.id"><td>{{item.id}}</td><td>{{item.title}}</td></tr>
   </tbody>
 </table>
@@ -28,11 +33,16 @@ export default {
     },
     async mounted() {
     // GET request using axios with async/await
-    await axios.get("https://adb-2166169465802843.3.azuredatabricks.net/?o=2166169465802843#mlflow/models/disease_management_recomm/serving",{
-        headers:{
-            Authorization: 'Bearer dapi6e073c785d1c6a2e4d0c1a7c40db1928-2'
+    const body = { "member_age": 59,"member_zipcode": "329090000","test_result": 9.6 };
+    await axios.post("https://adb-2166169465802843.3.azuredatabricks.net/model/disease_management_recomm/4/invocations", 
+        body,
+        {
+            headers: {
+                'Authorization': 'Bearer dapi6e073c785d1c6a2e4d0c1a7c40db1928-2',
+                'Content-Type':"application/json",
+            }
         }
-    })
+    )
     .then((res) => {
         this.list = res.data
         console.warn(res)
